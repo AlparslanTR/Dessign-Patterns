f32d159669c173086bbea145292f9d3344e76e6f
Alparslan AKBAŞ <bybluestht@gmail.com>
2023-05-26 20:00:24 +0300

    Design Pattern kullanımı için ufak identity sistemi kuruldu.

---
+++ Base.Main/Models/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Base.Main.Models.Data
+{
+    public class AppDbContext :IdentityDbContext<Customer>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
